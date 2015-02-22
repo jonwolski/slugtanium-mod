b9046e29bd84fd30a39a7951656d7eb846ffd75f
Jon Wolski <jonwolski@gmail.com>
2015-02-22 16:32:17 -0600

    Add sword

--- init.lua
+++ init.lua
@@ -19,15 +19,6 @@ minetest.register_node("slugtanium:stone_with_slugtanium", {
   sounds = default.node_sound_stone_defaults(),
 })
 
-minetest.register_craft({
-  output = 'slugtanium:block',
-  recipe = {
-      {'slugtanium:lump', 'slugtanium:lump', 'slugtanium:lump'},
-      {'slugtanium:lump', 'slugtanium:lump', 'slugtanium:lump'},
-      {'slugtanium:lump', 'slugtanium:lump', 'slugtanium:lump'}
-  }
-})
-
 minetest.register_ore({
   ore_type       = "scatter",
   ore            = "slugtanium:stone_with_slugtanium",
@@ -38,6 +29,37 @@ minetest.register_ore({
   height_min     = -31000,
   height_max     = 2400,
   flags          = "absheight",
+})
+
+minetest.register_tool("slugtanium:sword", {
+	description = "Slugtanium Sword",
+	inventory_image = "slugtanium_tool_sword.png",
+	tool_capabilities = {
+		full_punch_interval = 0.7,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=2.0, [2]=1.00, [3]=0.35}, uses=30, maxlevel=3},
+		},
+		damage_groups = {fleshy=7},
+	}
+})
+
+minetest.register_craft({
+  output = 'slugtanium:block',
+  recipe = {
+      {'slugtanium:lump', 'slugtanium:lump', 'slugtanium:lump'},
+      {'slugtanium:lump', 'slugtanium:lump', 'slugtanium:lump'},
+      {'slugtanium:lump', 'slugtanium:lump', 'slugtanium:lump'}
+  }
+})
+
+minetest.register_craft({
+  output = 'slugtanium:sword',
+  recipe = {
+      {'', 'slugtanium:block', ''},
+      {'', 'slugtanium:block', ''},
+      {'', 'default:stick', ''}
+  }
 })
 
 minetest.register_alias("slugtanium","slugtanium:block")
