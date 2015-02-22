ae2adad19f6e31f50da910c0b50b96e6faf52975
Jon Wolski <jonwolski@gmail.com>
2015-02-22 09:58:04 -0600

    Add slugtanium:block, texture, and alias

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ minetest.register_node("slugtanium:block", {
   tiles = {"slugtanium_block.png"},
   groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2}
 })
+
+minetest.register_alias("slugtanium","slugtanium:block")
